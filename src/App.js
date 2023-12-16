9dc9d3ec64ccff6742bcc3876bb134ca00b688ce
TpRaphulu <tpraphulu@gmail.com>
2023-12-17 00:52:40 +0200

    Added Bootsrap

--- src/App.js
+++ src/App.js
@@ -4,21 +4,23 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather app</h1>
-      <footer>
-        This project was code by{" "}
-        <a href="" target="_blank">
-          Thabiso Raphulu
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/TpRaphulu/react-weather-app"
-          target="_blank"
-        >
-    
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div class="container">
+        <h1> Weather app</h1>
+        <button className="btn btn-primary"> hellow</button>
+        <footer>
+          This project was code by{" "}
+          <a href="" target="_blank">
+            Thabiso Raphulu
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/TpRaphulu/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
