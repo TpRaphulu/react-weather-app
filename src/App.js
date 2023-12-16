fe47d640e03d0857cd9bb3fac5a45bc72354d402
TpRaphulu <tpraphulu@gmail.com>
2023-12-17 00:24:59 +0200

    added github url

--- src/App.js
+++ src/App.js
@@ -4,8 +4,22 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      weather app 
-      </div> 
+      <h1> Weather app</h1>
+      <footer>
+        This project was code by{" "}
+        <a href="" target="_blank">
+          Thabiso Raphulu
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/TpRaphulu/react-weather-app"
+          target="_blank"
+        >
+    
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
   );
 }
 
