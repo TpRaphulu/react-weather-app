9c4cefb285253752ad49f5db171fa1ca3fddef86
TpRaphulu <tpraphulu@gmail.com>
2024-01-26 03:14:37 +0200

    nonerel

--- src/App.js
+++ src/App.js
@@ -9,23 +9,19 @@ export default function App() {
         <Weather defaultCity="Johannesburg" />
         <footer>
           This Project was code by{" "}
-          <a href="" target="_blank">
+          <a rel="noreferrer" href="" target="_blank">
             TP Raphulu
           </a>{" "}
           and is{" "}
           <a
+            rel="noreferrer"
             href="https://github.com/TpRaphulu/react-weather-app"
             target="_blank"
           >
             Open-Sourced on GitHub
-          </a>{" "}
-          and <a href="https://spectacular-marshmallow-348d2e.netlify.app/">
-            hosted on Netlify 
           </a>
         </footer>
       </div>
     </div>
   );
 }
-
-
