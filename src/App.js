42093ab7d37296e20461e3b9733cd92de7cf0d7e
TpRaphulu <tpraphulu@gmail.com>
2024-01-28 22:38:58 +0200

    ADDED NONREL

--- src/App.js
+++ src/App.js
@@ -5,23 +5,24 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <div class="container">
-        <Weather defaultCity="Johannesburg" />
-        <footer>
-          This Project was code by{" "}
-          <a rel="noreferrer" href="" target="_blank">
-            TP Raphulu
-          </a>{" "}
-          and is{" "}
-          <a
-            rel="noreferrer"
-            href="https://github.com/TpRaphulu/react-weather-app"
-            target="_blank"
-          >
-            Open-Sourced on GitHub
-          </a>
-        </footer>
-      </div>
+      <h1>React Weather App</h1>
+      This project was coded by{" "}
+      <a
+        rel="noreferrer"
+        href="https://magenta-khapse-6f3ded.netlify.app/"
+        target="_blank"
+      >
+        Thabiso Raphulu
+      </a>{" "}
+      and is
+      <a
+        rel="noreferrer"
+        href="https://github.com/TpRaphulu/react-weather-app"
+        target="_blank"
+      >
+        {" "}
+        open-sourced on GitHub
+      </a>
     </div>
   );
 }
